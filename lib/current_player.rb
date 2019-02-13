e10127fc3ad63f8eac52a990d55811397fd9b8eb
Brian Simonton <bsimonton78@gmail.com>
2019-02-13 03:49:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,16 +3,14 @@
 
 def turn_count(board)
 count = 0
-turns = []
 
 board.each do |space|
-  if space != " " || ""
-  count += 1
-else
-  turn_count(board)
+  if space = "X" || "O"
+    count +=1
+  end
 end
 
-
+count
 
 
 end
